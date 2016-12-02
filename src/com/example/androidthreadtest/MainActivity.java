6ed7d9be4a00e9e845c2c91ddb6e2229b0e22bb3
dzy <dzymt6627@163.com>
2016-12-02 19:27:31 +0800

    asynchronous message handling

--- src/com/example/androidthreadtest/MainActivity.java
+++ src/com/example/androidthreadtest/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.androidthreadtest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,8 +14,23 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity implements OnClickListener {
 
+	public static final int UPDATE_TEXT = 1;
 	private TextView text;
 	private Button changeText;
+	
+	private Handler handler = new Handler() {
+	
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case UPDATE_TEXT:
+				//在这里可以进行UI操作
+				text.setText("Nice to meet you");
+				break;
+			default:
+				break;
+			}
+		}
+	};
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +47,14 @@ public class MainActivity extends Activity implements OnClickListener {
 		switch (v.getId()) {
 		case R.id.change_text:
 			new Thread(new Runnable() {
-				
+				//在子线程中执行耗时任务，根据任务的执行结果来更新相应的UI控件
 				@Override
 				public void run() {
 					// TODO Auto-generated method stub
-					text.setText("Nice to meet you");
+//					text.setText("Nice to meet you");
+					Message message = new Message();
+					message.what = UPDATE_TEXT;
+					handler.sendMessage(message); //将Message对象发送出去
 				}
 			}).start();
 			break;
